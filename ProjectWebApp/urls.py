bf073633c4d8ac9b9187d9dcf4695ac620c89914
Mario Ramb <marioramb@gmail.com>
2021-08-18 10:09:01 -0300

    first commit

---
+++ ProjectWebApp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from ProjectWebApp import views
+
+urlpatterns = [
+    path('', views.home, name = "Home"),
+    path('services', views.services, name = "Services"),
+    path('shop', views.shop, name = "Shop"),
+    path('blog', views.blog, name = "Blog"),
+    path('contact', views.contact, name = "Contact"),
+]
